50a340c6d66d1fc46b0ed86f0c78cbdd53155697
lina <lina@weversecompany.com>
2021-08-26 22:58:30 +0900

    Add: 프로젝트 파일 생성

---
+++ app-store-search-tap-clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  app-store-search-tap-clone
+//
+//  Created by lina on 2021/08/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
