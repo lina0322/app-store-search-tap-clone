86e9b92694d128c15faef9055741306b5a36996a
lina <lina@weversecompany.com>
2021-08-26 22:59:10 +0900

    Feat: 모델 타입 구현

---
+++ app-store-search-tap-clone/Model/Config.swift
@@ -0,0 +1,12 @@
+//
+//  Config.swift
+//  app-store-search-tap-clone
+//
+//  Created by lina on 2021/08/26.
+//
+
+import Foundation
+
+enum Config {
+    static let baseURL = "https://itunes.apple.com/search"
+}
---
+++ app-store-search-tap-clone/Model/SearchItem.swift
@@ -0,0 +1,20 @@
+//
+//  SearchItem.swift
+//  app-store-search-tap-clone
+//
+//  Created by lina on 2021/08/26.
+//
+
+import Foundation
+
+struct SearchItem: Encodable {
+    let term: String
+    var country: String = "kr"
+    var media: String = "software"
+    
+    var parameters: [String : String] {[
+        "term": term,
+        "country": country,
+        "media": media,
+    ]}
+}
